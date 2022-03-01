f725476a68119fb4256c67fbbaee74cac7f22e76
Miroslav Zaitsev <m1r1k25@mail.ru>
2022-03-01 16:44:27 +0300

    feat: start using svg graphics

--- my-app-weather-widget/src/Components/WeatherPage/WeatherPage.jsx
+++ my-app-weather-widget/src/Components/WeatherPage/WeatherPage.jsx
@@ -12,18 +12,32 @@ const WeatherPage = (props) => {
          <div className={styles.iconContainer}>
             <img src={require(`./../../assets/images/${item.cloudImg}`)}></img>
          </div>
-         <div className={styles.emptyContainer}></div>
-         <div className={styles.emptyContainer}></div>
+         <div className={styles.emptyContainer1}><div className={styles.round}>0</div></div>
+         <div className={styles.emptyContainer2}><div className={styles.round}>0</div></div>
          <div className={styles.iconContainer}>
             <img src={require(`./../../assets/images/${item.moonImg}`)}></img>
          </div>
-         <div className={styles.windSpeedContainer}>${item.wind}</div>
+         <div className={styles.windSpeedContainer}>
+            <span>↗ {item.wind}</span>
+         </div>
       </div>
    })
-
+   //<div className={styles.round}>asfsaf</div>
    return (
       <div className={styles.weatherPageContainer}>
          {elements}
+         <div className={styles.graphic1}>
+            <svg className={styles.svg}>
+               <polyline points="120,60 360,60 600,40 840, 40 1080, 60"
+                  fill="none" stroke="white" stroke-width="3" />
+            </svg>
+         </div>
+         <div className={styles.graphic2}>
+            <svg className={styles.svg}>
+               <polyline points="120,60 360,40 600,40 840, 60 1080, 60"
+                  fill="none" stroke="white" stroke-width="3" />
+            </svg>
+         </div>
       </div>
    )
 }
